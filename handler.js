bd956d348c6aad32ae01c26b371389f9fd0bcbf0
Ketan <ksaxena@gammastack.com>
2020-04-08 20:11:18 +0530

    Update env variable name

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ AWS.config.update({region : 'ap-south-1'});
 const rds = new AWS.RDS({ apiVersion: '2014-10-31' });
 
 const toggleDB = (event, action, callback) => {
-  const instanceId = event.instanceId || process.env.INSANCE_ID;
+  const instanceId = event.instanceId || process.env.INSTANCE_ID;
   const handler = action === 'stop' ? 'stopDBInstance' : 'startDBInstance'; 
   rds[handler]({ DBInstanceIdentifier: instanceId }, (err, data) => {
     console.log(`action completed: ${ err || data }`)
